389599384c5eacefb428e5a991285fe687df2542
JuLblc <julien.leblanc@epfedu.fr>
2022-01-17 18:10:34 +0100

    add spinner loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,41 @@
 import '../styles/globals.css'
 
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
 
 function MyApp ({ Component, pageProps }) {
   // console.log('myApp: ', pageProps)
+
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
+  //Spinner loading
+  useEffect(() => {
+    const handleStart = url => {
+      url !== router.pathname ? setLoading(true) : setLoading(false)
+    }
+    const handleComplete = url => setLoading(false)
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+  }, [router])
+
   return (
-      <SessionProvider session={pageProps.session}>
-        <Navbar />
-        <Component {...pageProps} />
-      </SessionProvider>
+    <SessionProvider session={pageProps.session}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Navbar />
+          <Component {...pageProps} />
+        </>
+      )}
+    </SessionProvider>
   )
 }
 
--- pages/profile.js
+++ pages/profile.js
@@ -1,36 +1,34 @@
-import { getSession } from "next-auth/react";
+import { getSession } from 'next-auth/react'
 
 const Profile = ({ data }) => {
-
-    return (
-        <>
-            <h1>Mon compte</h1>
-            <h2>{data}</h2>
-        </>
-    );
+  return (
+    <>
+      <h1>Mon compte</h1>
+      <h2>{data}</h2>
+    </>
+  )
 }
 
-export default Profile;
+export default Profile
 
 //Server side rendering
-export const getServerSideProps = async (context) => {
+export const getServerSideProps = async context => {
+  const session = await getSession(context)
+  // console.log('session getServer Profile: ', session)
 
-    const session = await getSession(context)
-    // console.log('session getServer Profile: ', session)
-
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/login',
-                permanent: false,
-            }
-        }
-    }
-
+  if (!session) {
     return {
-        props: {
-            session,
-            data: session ? `Hello ${session.user.firstName}` : ''
-        }
+      redirect: {
+        destination: '/login',
+        permanent: false
+      }
     }
+  }
+
+  return {
+    props: {
+      session,
+      data: session ? `Hello ${session.user.firstName}` : ''
+    }
+  }
 }
