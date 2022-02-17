baf5011cf6fc0696ff89a7baf3e1b1fe5f735770
Julien Leblanc <julien.leblanc@epfedu.fr>
2022-02-17 16:34:16 +0100

    css lecture

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,41 +1,40 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import logo from '../public/logo.png'
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../public/logo.png";
 
-import { signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
-import { useWindowDimensions } from '../hooks/useWindowDimensions'
-import { useScrollPosition } from '../hooks/useScrollPosition'
-import { useScrollY } from '../hooks/useScrollY'
-import { getDeviceTypeInfo } from '../utils/utilResponsive'
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
+import { useScrollPosition } from "../hooks/useScrollPosition";
+import { useScrollY } from "../hooks/useScrollY";
+import { getDeviceTypeInfo } from "../utils/utilResponsive";
 
-import Burger from './Burger'
+import Burger from "./Burger";
 
-import styles from '../styles/Navbar.module.css'
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
-  const { data: session, status } = useSession()
-  const router = useRouter()
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-  const { width, height } = useWindowDimensions()
+  const { width, height } = useWindowDimensions();
 
-  const goingUp = useScrollY()
-  const scrollPositionY = useScrollPosition()
-  console.log(goingUp, scrollPositionY)
+  const goingUp = useScrollY();
+  const scrollPositionY = useScrollPosition();
 
   const {
     isMobile,
     isTablet,
     isDesktopOrLaptop,
     isBigScreen
-  } = getDeviceTypeInfo(width, height)
+  } = getDeviceTypeInfo(width, height);
 
-  const handleSignOut = e => {
-    e.preventDefault()
+  const handleSignOut = (e) => {
+    e.preventDefault();
     signOut({
-      callbackUrl: '/login?login=signin'
-    })
-  }
+      callbackUrl: "/login?login=signin"
+    });
+  };
 
   return (
     <>
@@ -44,39 +43,39 @@ const Navbar = () => {
           <div className={styles.header}>
             <ul
               className={`${styles.headerNav} ${
-                status === 'loading' ? styles.loading : styles.loaded
+                status === "loading" ? styles.loading : styles.loaded
               } `}
             >
-              <Link href='/'>
+              <Link href="/">
                 <span className={styles.imageWrapper}>
-                  <Image src={logo} alt='logo' />
+                  <Image src={logo} alt="logo" />
                 </span>
               </Link>
               {/* Display menu according to device */}
               {(isDesktopOrLaptop || isBigScreen) && (
                 <>
                   <li>
-                    <Link href='/courses'>
+                    <Link href="/courses">
                       <a>Formations</a>
                     </Link>
                   </li>
                   <li>
-                    <Link href='/exercices'>
+                    <Link href="/exercices">
                       <a>Exercices</a>
                     </Link>
                   </li>
                   <li>
-                    <Link href='/blog'>
+                    <Link href="/blog">
                       <a>Blog</a>
                     </Link>
                   </li>
                   <li>
-                    <Link href='#'>
+                    <Link href="#">
                       <a>Prestation</a>
                     </Link>
                   </li>
                   <li>
-                    <Link href='#'>
+                    <Link href="#">
                       <a>A propos</a>
                     </Link>
                   </li>
@@ -85,23 +84,23 @@ const Navbar = () => {
             </ul>
             <ul
               className={`${styles.headerRightSide} ${
-                status === 'loading' ? styles.loading : styles.loaded
+                status === "loading" ? styles.loading : styles.loaded
               }`}
             >
               {/* If user is logged in -> display profile button*/}
               {session && (
                 <li
                   className={styles.liRightSide}
-                  onClick={() => router.push('/profile')}
+                  onClick={() => router.push("/profile")}
                 >
                   <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 24 24'
-                    width='16'
-                    height='16'
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="16"
+                    height="16"
                   >
-                    <path fill='none' d='M0 0h24v24H0z' />
-                    <path d='M4 22a8 8 0 1 1 16 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z' />
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M4 22a8 8 0 1 1 16 0H4zm8-9c-3.315 0-6-2.685-6-6s2.685-6 6-6 6 2.685 6 6-2.685 6-6 6z" />
                   </svg>
                   {(isDesktopOrLaptop || isBigScreen) && (
                     <div className={styles.divRightSide}>
@@ -111,33 +110,33 @@ const Navbar = () => {
                 </li>
               )}
               {/* If user is not logged in -> display login & sign up link*/}
-              {status === 'unauthenticated' && !session && (
+              {status === "unauthenticated" && !session && (
                 /* Display link according to device */
 
                 <>
                   {isTablet || isMobile ? (
                     <li className={styles.liRightSide}>
-                      <Link href='/login?login=signin'>
+                      <Link href="/login?login=signin">
                         <svg
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 24 24'
-                          width='18'
-                          height='18'
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          width="18"
+                          height="18"
                         >
-                          <path fill='none' d='M0 0h24v24H0z' />
-                          <path d='M10 11V8l5 4-5 4v-3H1v-2h9zm-7.542 4h2.124A8.003 8.003 0 0 0 20 12 8 8 0 0 0 4.582 9H2.458C3.732 4.943 7.522 2 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10c-4.478 0-8.268-2.943-9.542-7z' />
+                          <path fill="none" d="M0 0h24v24H0z" />
+                          <path d="M10 11V8l5 4-5 4v-3H1v-2h9zm-7.542 4h2.124A8.003 8.003 0 0 0 20 12 8 8 0 0 0 4.582 9H2.458C3.732 4.943 7.522 2 12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10c-4.478 0-8.268-2.943-9.542-7z" />
                         </svg>
                       </Link>
                     </li>
                   ) : (
                     <>
                       <li className={`${styles.btnLogin} ${styles.btnSignIn}`}>
-                        <Link href='/login?login=signin'>
+                        <Link href="/login?login=signin">
                           <a>Se connecter</a>
                         </Link>
                       </li>
                       <li className={`${styles.btnLogin} ${styles.btnSignUp}`}>
-                        <Link href='/login?login=signup'>
+                        <Link href="/login?login=signup">
                           <a>S'inscrire</a>
                         </Link>
                       </li>
@@ -149,16 +148,16 @@ const Navbar = () => {
               {/* If user is logged in -> display logout button*/}
               {session && (
                 <li className={styles.liRightSide}>
-                  <Link href='/api/auth/signout'>
+                  <Link href="/api/auth/signout">
                     <a onClick={handleSignOut}>
                       <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        viewBox='0 0 24 24'
-                        width='16'
-                        height='16'
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="16"
+                        height="16"
                       >
-                        <path fill='none' d='M0 0h24v24H0z' />
-                        <path d='M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z' />
+                        <path fill="none" d="M0 0h24v24H0z" />
+                        <path d="M5 22a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v3h-2V4H6v16h12v-2h2v3a1 1 0 0 1-1 1H5zm13-6v-3h-7v-2h7V8l5 4-5 4z" />
                       </svg>
                     </a>
                   </Link>
@@ -177,7 +176,7 @@ const Navbar = () => {
         </nav>
       )}
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/courses/[courseUrl]/lecture/[lectureUrl].js
+++ pages/courses/[courseUrl]/lecture/[lectureUrl].js
@@ -1,48 +1,48 @@
-import { getSession } from 'next-auth/react'
-import Image from 'next/image'
+import { getSession } from "next-auth/react";
+import Image from "next/image";
 
-import Vimeo from '@u-wave/react-vimeo'
+import Vimeo from "@u-wave/react-vimeo";
 
-import { useWindowDimensions } from '../../../../hooks/useWindowDimensions'
-import { getDeviceTypeInfo } from '../../../../utils/utilResponsive'
-import { getCourses } from '../../../../apiCall'
-import { getUser } from '../../../../apiCall'
+import { useWindowDimensions } from "../../../../hooks/useWindowDimensions";
+import { getDeviceTypeInfo } from "../../../../utils/utilResponsive";
+import { getCourses } from "../../../../apiCall";
+import { getUser } from "../../../../apiCall";
 
-import SideCourseChapter from '../../../../components/SideCourseChapter'
+import SideCourseChapter from "../../../../components/SideCourseChapter";
 
-import styles from '../../../../styles/Lectures.module.css'
+import styles from "../../../../styles/Lectures.module.css";
 
-import blockedImg from '../../../../public/blocked.jpg'
-import { useEffect, useState } from 'react'
+import blockedImg from "../../../../public/blocked.jpg";
+import { useEffect, useState } from "react";
 
-const Lectures = props => {
-  const { width, height } = useWindowDimensions()
+const Lectures = (props) => {
+  const { width, height } = useWindowDimensions();
 
   const {
     isMobile,
     isTablet,
     isDesktopOrLaptop,
     isBigScreen
-  } = getDeviceTypeInfo(width, height)
+  } = getDeviceTypeInfo(width, height);
 
-  const [toggle, setToggle] = useState(true) // true => description / false => sommaire
+  const [toggle, setToggle] = useState(true); // true => description / false => sommaire
 
   useEffect(() => {
     if (isDesktopOrLaptop || isBigScreen) {
-      setToggle(true)
+      setToggle(true);
     }
-  }, [width, height])
+  }, [width, height]);
 
   return (
     <main>
       <h1>{props.lecture.title}</h1>
-      <div className={styles.container}>
+      <section className={styles.container}>
         {props.course.isPurchased ? (
           <Vimeo video={props.lecture.url} responsive={true} />
         ) : (
           <div className={styles.blockedContentWrapper}>
             <div className={styles.blockedImageWrapper}>
-              <Image src={blockedImg} alt='blockedImg' />
+              <Image src={blockedImg} alt="blockedImg" />
             </div>
             <span className={styles.blockedContent}>
               Achetez la formation pour visionner la vidéo
@@ -54,55 +54,56 @@ const Lectures = props => {
             <SideCourseChapter course={props.course} styles={styles} />
           </div>
         )}
-      </div>
-      <div className={styles.break}></div>
-      {(isMobile || isTablet) && (
-        <>
-          <div className={styles.smallNav}>
-            <div
-              className={toggle && `${styles.selected}`}
-              onClick={() => !toggle && setToggle(!toggle)}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                width='24'
-                height='24'
+      </section>
+      {/* <div className={styles.break}></div> */}
+      <section className={styles.lectureContainer}>
+        {(isMobile || isTablet) && (
+          <>
+            <div className={styles.break}></div>
+            <div className={styles.smallNav}>
+              <div
+                className={toggle && `${styles.selected}`}
+                onClick={() => !toggle && setToggle(!toggle)}
               >
-                <path fill='none' d='M0 0h24v24H0z' />
-                <path
-                  d='M12.9 6.858l4.242 4.243L7.242 21H3v-4.243l9.9-9.9zm1.414-1.414l2.121-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z'
-                  // fill='rgba(58,58,58,1)'
-                  fill={toggle ? '#0085ff' : 'rgba(58,58,58,1)'}
-                />
-              </svg>
-              <span>Description</span>
-            </div>
-            <div
-              className={!toggle && `${styles.selected}`}
-              onClick={() => toggle && setToggle(!toggle)}
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                width='24'
-                height='24'
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12.9 6.858l4.242 4.243L7.242 21H3v-4.243l9.9-9.9zm1.414-1.414l2.121-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z"
+                    // fill='rgba(58,58,58,1)'
+                    fill={toggle ? "#0085ff" : "rgba(58,58,58,1)"}
+                  />
+                </svg>
+                <span>Description</span>
+              </div>
+              <div
+                className={!toggle && `${styles.selected}`}
+                onClick={() => toggle && setToggle(!toggle)}
               >
-                <path fill='none' d='M0 0h24v24H0z' />
-                <path
-                  d='M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zm-2 3V4H4v15a1 1 0 0 0 1 1h11zM6 7h8v2H6V7zm0 4h8v2H6v-2zm0 4h5v2H6v-2z'
-                  // fill='rgba(58,58,58,1)'
-                  fill={!toggle ? '#0085ff' : 'rgba(58,58,58,1)'}
-                />
-              </svg>
-              <span>Sommaire</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zm-2 3V4H4v15a1 1 0 0 0 1 1h11zM6 7h8v2H6V7zm0 4h8v2H6v-2zm0 4h5v2H6v-2z"
+                    // fill='rgba(58,58,58,1)'
+                    fill={!toggle ? "#0085ff" : "rgba(58,58,58,1)"}
+                  />
+                </svg>
+                <span>Sommaire</span>
+              </div>
             </div>
-          </div>
-          <div className={styles.break}></div>
-        </>
-      )}
+            <div className={styles.break}></div>
+          </>
+        )}
 
-      <div className={styles.lectureContainer}>
         {toggle && (
           <div className={styles.lectureDescription}>
             <h2>A propos de ce contenu</h2>
@@ -112,58 +113,60 @@ const Lectures = props => {
         {(isMobile || isTablet) && !toggle && (
           <SideCourseChapter course={props.course} styles={styles} />
         )}
-      </div>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Lectures
+export default Lectures;
 
 //Server side rendering
-export const getServerSideProps = async context => {
-  const session = await getSession(context)
-  const { chapter, lectureUrl } = context.query
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  const { chapter, lectureUrl } = context.query;
 
-  const resCourse = await getCourses(context, context.query.courseUrl)
+  const resCourse = await getCourses(context, context.query.courseUrl);
   // console.log('resCourse: ', resCourse.data.courseFromDB)
 
   // Check if user already purchased this course. Pass the result as props
-  const course = resCourse.data.courseFromDB
+  const course = resCourse.data.courseFromDB;
 
   // Display only the current lecture
   const currentChapter = resCourse.data.courseFromDB.chapters.filter(
-    chapt => chapt.seoUrl === chapter
-  )[0]
+    (chapt) => chapt.seoUrl === chapter
+  )[0];
   const lecture = currentChapter.lectures.filter(
-    lecture => lecture.seoUrl === lectureUrl
-  )[0]
+    (lecture) => lecture.seoUrl === lectureUrl
+  )[0];
 
   if (!session) {
-    course.isPurchased = false
+    course.isPurchased = false;
     return {
       props: {
         session,
         course,
         lecture
       }
-    }
+    };
   }
 
-  if (session.user.role === 'ADMIN') {
-    course.isPurchased = true
+  if (session.user.role === "ADMIN") {
+    course.isPurchased = true;
     return {
       props: {
         session,
         course,
         lecture
       }
-    }
+    };
   }
 
-  const resUser = await getUser(context)
-  const purchasedCourses = resUser.data.userFromDB.purchasedCourses
+  const resUser = await getUser(context);
+  const purchasedCourses = resUser.data.userFromDB.purchasedCourses;
 
-  purchasedCourses.indexOf(course._id) === -1 ? course.isPurchased = false : course.isPurchased = true
+  purchasedCourses.indexOf(course._id) === -1
+    ? (course.isPurchased = false)
+    : (course.isPurchased = true);
 
   return {
     props: {
@@ -171,5 +174,5 @@ export const getServerSideProps = async context => {
       course,
       lecture
     }
-  }
-}
+  };
+};
