ef26cce26ada37867c189b3a67f491ec36cfd544
JuLblc <julien.leblanc@epfedu.fr>
2022-03-04 19:17:23 +0100

    test quote cardtesti

--- components/CardTestimonial.js
+++ components/CardTestimonial.js
@@ -3,10 +3,15 @@ import styles from '../styles/CardTestimonial.module.css'
 
 const CardTestimonial = props => {
   return (
-    <div className={`${styles.cardContainer} ${!props.display && styles.notDisplayed}`}>
+    <div
+      className={`${styles.cardContainer} ${!props.display &&
+        styles.notDisplayed}`}
+    >
       <div className={styles.imageWrapper}>
         <Image src={props.img} alt='testimonial-picture' />
-        <div className={styles.quote}>"</div>
+        <div className={styles.quoteContainer}>
+          <div className={styles.quote}>"</div>
+        </div>
       </div>
 
       <p className={styles.testimonial}>
