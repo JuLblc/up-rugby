2417edbf2193831da68afe20742359bb5599c875
Julien Leblanc <julien.leblanc@epfedu.fr>
2022-03-14 17:55:38 +0100

    start new style Card Formation

--- components/CardFormation.js
+++ components/CardFormation.js
@@ -1,16 +1,24 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import img from '../public/relance-de-jeu.jpg'
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/CardFormation.module.css";
 
-import styles from '../styles/CardFormation.module.css'
+const CardFormation = (props) => {
+  const [extend, setExtend] = useState(false);
 
-const CardFormation = props => {
+  const handleClick = () => {
+    setExtend(!extend);
+  };
 
   return (
     <article className={styles.cardFormation}>
       <span className={styles.imgCard}>
-        {/* <Image src={img} alt='img' /> */}
-        <Image src={props.img.url} alt='img' width={props.img.width} height={props.img.height} />
+        <Image
+          src={props.img.url}
+          alt="img"
+          width={props.img.width}
+          height={props.img.height}
+        />
       </span>
       <div className={styles.CardFormationWrapper}>
         <div className={styles.formationInfo}>
@@ -18,55 +26,99 @@ const CardFormation = props => {
           <p className={styles.formationDescription}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
+          <div className={styles.cardInfoWrapper}>
+            <div className={styles.cardInfoContainer}>
+              <div className={styles.cardInfo}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="19"
+                  height="19"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zm-2 3V4H4v15a1 1 0 0 0 1 1h11zM6 7h8v2H6V7zm0 4h8v2H6v-2zm0 4h5v2H6v-2z"
+                    fill="rgba(128,128,128,1)"
+                  />
+                </svg>
+                <span>{`${props.lecturesQty} Chapitre${
+                  props.lecturesQty > 1 ? "s" : ""
+                }`}</span>
+              </div>
+              <div className={styles.cardInfo}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="19"
+                  height="19"
+                >
+                  <path fill="none" d="M0 0h24v24H0z" />
+                  <path
+                    d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8h4v2h-6V7h2v5z"
+                    fill="rgba(128,128,128,1)"
+                  />
+                </svg>
+                <span>{props.lecturesTimes}</span>
+              </div>
+            </div>
+            <div className={styles.priceContainer}>
+              <div className={styles.price}>
+                {props.price}
+                <span>€</span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.CTA}>
+            <Link href={`/courses/${props.seoUrl}`}>
+              <a className={styles.linkDetails}>Détails</a>
+            </Link>
+            <button>Acheter</button>
+          </div>
         </div>
-        <div className={styles.cardFooterContainer}>
-          <div className={styles.cardFooterInfo}>
+        <div className={styles.cardFooterContainer} onClick={handleClick}>
+          {!extend && (
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='19'
-              height='19'
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
             >
-              <path fill='none' d='M0 0h24v24H0z' />
+              <path fill="none" d="M0 0h24v24H0z" />
               <path
-                d='M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zm-2 3V4H4v15a1 1 0 0 0 1 1h11zM6 7h8v2H6V7zm0 4h8v2H6v-2zm0 4h5v2H6v-2z'
-                fill='rgba(128,128,128,1)'
+                d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"
+                fill="rgba(128,128,128,1)"
               />
             </svg>
-            <span>{`${props.lecturesQty} Chapitre${
-              props.lecturesQty > 1 ? 's' : ''
-            }`}</span>
-          </div>
-          <div className={styles.cardFooterInfo}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='19'
-              height='19'
-            >
-              <path fill='none' d='M0 0h24v24H0z' />
-              <path
-                d='M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8h4v2h-6V7h2v5z'
-                fill='rgba(128,128,128,1)'
-              />
-            </svg>
-            <span>{props.lecturesTimes}</span>
-          </div>
+          )}
 
-          <span>{props.price} €</span>
+          {extend && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path
+                d="M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"
+                fill="rgba(128,128,128,1)"
+              />
+            </svg>
+          )}
+
           {/* if course is a draft, it still can be updated by ADMIN only */}
-          {props.role === 'ADMIN' && !props.isPublished && (
+          {props.role === "ADMIN" && !props.isPublished && (
             <Link href={`/courses/update-course/${props.seoUrl}`}>
               <a className={styles.linkAdmin}>Modifier</a>
             </Link>
           )}
         </div>
       </div>
-      <Link href={`/courses/${props.seoUrl}`}>
+      {/* <Link href={`/courses/${props.seoUrl}`}>
         <a className={styles.link}>Détails</a>
-      </Link>
+      </Link> */}
     </article>
-  )
-}
+  );
+};
 
-export default CardFormation
+export default CardFormation;
--- pages/api/courses/index.js
+++ pages/api/courses/index.js
@@ -1,121 +1,121 @@
-const { connectToDatabase } = require('../../../utils/mongodb')
+const { connectToDatabase } = require("../../../utils/mongodb");
 
-import Course from '../../../models/Course.model'
+import Course from "../../../models/Course.model";
 
-import { getSession } from 'next-auth/react'
+import { getSession } from "next-auth/react";
 
-export default async function handler (req, res) {
-  const session = await getSession({ req })
-  const { method, query } = req
+export default async function handler(req, res) {
+  const session = await getSession({ req });
+  const { method, query } = req;
 
   // console.log('session API: ', session)
 
   connectToDatabase()
     .then(() => {
       switch (method) {
-        case 'GET':
+        case "GET":
           if (!query.url) {
-            getAllCourses(req, res, session)
-            break
+            getAllCourses(req, res, session);
+            break;
           }
 
-          getCourse(req, res, session)
-          break
+          getCourse(req, res, session);
+          break;
 
-        case 'POST':
-          addCourse(req, res, session)
-          break
-        case 'PUT':
-          updateCourse(req, res, session)
-          break
-        case 'DELETE':
-          deleteCourse(req, res, session)
-          break
+        case "POST":
+          addCourse(req, res, session);
+          break;
+        case "PUT":
+          updateCourse(req, res, session);
+          break;
+        case "DELETE":
+          deleteCourse(req, res, session);
+          break;
       }
     })
-    .catch(err => {
-      console.log(err)
+    .catch((err) => {
+      console.log("ERR: ", err);
       res
         .status(400)
-        .json({ message: 'La connexion à la base de donnée a échoué' })
-    })
+        .json({ message: "La connexion à la base de donnée a échoué" });
+    });
 }
 
 const addCourse = (req, res, session) => {
   //console.log('req: ', req)
-  const { course } = req.body
+  const { course } = req.body;
 
-  if (!session || session.user.role !== 'ADMIN') {
-    res.status(401).json({ message: 'Unauthorized' })
-    return
+  if (!session || session.user.role !== "ADMIN") {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
   }
 
-  const newCourse = new Course(course)
+  const newCourse = new Course(course);
 
   newCourse
     .save()
-    .then(newCourseFromDB => {
-      res.status(200).json({ newCourseFromDB })
+    .then((newCourseFromDB) => {
+      res.status(200).json({ newCourseFromDB });
     })
-    .catch(err => console.log('err : ', err))
-}
+    .catch((err) => console.log("err : ", err));
+};
 
 const getAllCourses = (req, res, session) => {
-  const cond = {}
-  if (!session || session.user.role !== 'ADMIN') {
-    cond.isPublished = true
+  const cond = {};
+  if (!session || session.user.role !== "ADMIN") {
+    cond.isPublished = true;
   }
 
   Course.find(cond)
     .sort({ _id: -1 }) //sort collection in descending order based on the date of insertion
-    .then(coursesFromDB => {
-      res.status(200).json({ coursesFromDB })
+    .then((coursesFromDB) => {
+      res.status(200).json({ coursesFromDB });
     })
-    .catch(err => console.log('err : ', err))
-}
+    .catch((err) => console.log("err : ", err));
+};
 
 const getCourse = (req, res, session) => {
-  const seoUrl = req.query.url
+  const seoUrl = req.query.url;
 
-  const cond = { seoUrl }
-  if (!session || session.user.role !== 'ADMIN') {
-    cond.isPublished = true
+  const cond = { seoUrl };
+  if (!session || session.user.role !== "ADMIN") {
+    cond.isPublished = true;
   }
 
   Course.findOne(cond)
-    .then(courseFromDB => {
-      res.status(200).json({ courseFromDB })
+    .then((courseFromDB) => {
+      res.status(200).json({ courseFromDB });
     })
-    .catch(err => console.log('err : ', err))
-}
+    .catch((err) => console.log("err : ", err));
+};
 
 const updateCourse = (req, res, session) => {
-  if (!session || session.user.role !== 'ADMIN') {
-    res.status(401).json({ message: 'Unauthorized' })
-    return
+  if (!session || session.user.role !== "ADMIN") {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
   }
 
-  const id = req.body.course._id
-  const updatedCourse = req.body.course
+  const id = req.body.course._id;
+  const updatedCourse = req.body.course;
 
   Course.findByIdAndUpdate(id, updatedCourse, { new: true })
-    .then(updatedCourseFromDB => {
-      res.status(200).json({ updatedCourseFromDB })
+    .then((updatedCourseFromDB) => {
+      res.status(200).json({ updatedCourseFromDB });
     })
-    .catch(err => console.log('err : ', err))
-}
+    .catch((err) => console.log("err : ", err));
+};
 
 const deleteCourse = (req, res, session) => {
-  if (!session || session.user.role !== 'ADMIN') {
-    res.status(401).json({ message: 'Unauthorized' })
-    return
+  if (!session || session.user.role !== "ADMIN") {
+    res.status(401).json({ message: "Unauthorized" });
+    return;
   }
 
-  const id = req.body
+  const id = req.body;
 
   Course.findByIdAndDelete(id)
     .then(() => {
-      res.status(200).json({ message: 'Formation supprimer' })
+      res.status(200).json({ message: "Formation supprimer" });
     })
-    .catch(err => console.log('err : ', err))
-}
+    .catch((err) => console.log("err : ", err));
+};
