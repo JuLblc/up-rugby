d29819dcb6716136eba5ba42e361ebfb4096dc1e
Julien Leblanc <julien.leblanc@epfedu.fr>
2022-03-16 09:15:27 +0100

    Style course Card

--- components/CardFormation.js
+++ components/CardFormation.js
@@ -1,50 +1,49 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { useState, useRef, useEffect } from 'react'
-import styles from '../styles/CardFormation.module.css'
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
+import styles from "../styles/CardFormation.module.css";
 
-import { useWindowDimensions } from '../hooks/useWindowDimensions'
+import { useWindowDimensions } from "../hooks/useWindowDimensions";
 
-import { getDeviceTypeInfo } from '../utils/utilResponsive'
+import { getDeviceTypeInfo } from "../utils/utilResponsive";
 
-const CardFormation = props => {
-  const { width, height } = useWindowDimensions()
-  const { isMobile, isTablet } = getDeviceTypeInfo(width, height)
+const CardFormation = (props) => {
+  const { width, height } = useWindowDimensions();
+  const { isMobile, isTablet } = getDeviceTypeInfo(width, height);
 
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(false);
 
-  const divRef = useRef()
+  const divRef = useRef();
 
   const handleSelectedChange = () => {
-    const $div = divRef.current.classList
-    console.log('slected: ', selected, 'div: ', $div)
-    
+    const $div = divRef.current.classList;
+
     if (selected) {
-      if ($div.contains('CardFormation_selected__IoyVY')) {
-        $div.remove('CardFormation_selected__IoyVY')
+      if ($div.contains("CardFormation_selected__IoyVY")) {
+        $div.remove("CardFormation_selected__IoyVY");
       }
-      $div.add('CardFormation_unselected__XRc3W')
+      $div.add("CardFormation_unselected__XRc3W");
     }
 
     if (!selected) {
-      if ($div.contains('CardFormation_unselected__XRc3W')) {
-        $div.remove('CardFormation_unselected__XRc3W')
+      if ($div.contains("CardFormation_unselected__XRc3W")) {
+        $div.remove("CardFormation_unselected__XRc3W");
       }
-      $div.add('CardFormation_selected__IoyVY')
+      $div.add("CardFormation_selected__IoyVY");
     }
-    setSelected(!selected)
-  }
+    setSelected(!selected);
+  };
 
   return (
     <article
       className={styles.cardFormation}
-      onMouseEnter={handleSelectedChange}
-      onMouseLeave={handleSelectedChange}
+      onMouseEnter={isTablet || isMobile ? undefined : handleSelectedChange}
+      onMouseLeave={isTablet || isMobile ? undefined : handleSelectedChange}
     >
       <span className={styles.imgCard}>
         <Image
           src={props.img.url}
-          alt='img'
+          alt="img"
           width={props.img.width}
           height={props.img.height}
         />
@@ -53,7 +52,7 @@ const CardFormation = props => {
         <div ref={divRef} className={styles.formationInfo}>
           <h3 className={styles.formationTitle}>{props.title}</h3>
           {/* if course is a draft, it still can be updated by ADMIN only */}
-          {props.role === 'ADMIN' && !props.isPublished && (
+          {props.role === "ADMIN" && !props.isPublished && (
             <Link href={`/courses/update-course/${props.seoUrl}`}>
               <a className={styles.linkAdmin}>Modifier</a>
             </Link>
@@ -65,32 +64,32 @@ const CardFormation = props => {
             <div className={styles.cardInfoContainer}>
               <div className={styles.cardInfo}>
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  width='19'
-                  height='19'
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="19"
+                  height="19"
                 >
-                  <path fill='none' d='M0 0h24v24H0z' />
+                  <path fill="none" d="M0 0h24v24H0z" />
                   <path
-                    d='M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zm-2 3V4H4v15a1 1 0 0 0 1 1h11zM6 7h8v2H6V7zm0 4h8v2H6v-2zm0 4h5v2H6v-2z'
-                    fill='rgba(128,128,128,1)'
+                    d="M19 22H5a3 3 0 0 1-3-3V3a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v12h4v4a3 3 0 0 1-3 3zm-1-5v2a1 1 0 0 0 2 0v-2h-2zm-2 3V4H4v15a1 1 0 0 0 1 1h11zM6 7h8v2H6V7zm0 4h8v2H6v-2zm0 4h5v2H6v-2z"
+                    fill="rgba(128,128,128,1)"
                   />
                 </svg>
                 <span>{`${props.lecturesQty} Chapitre${
-                  props.lecturesQty > 1 ? 's' : ''
+                  props.lecturesQty > 1 ? "s" : ""
                 }`}</span>
               </div>
               <div className={styles.cardInfo}>
                 <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  viewBox='0 0 24 24'
-                  width='19'
-                  height='19'
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="19"
+                  height="19"
                 >
-                  <path fill='none' d='M0 0h24v24H0z' />
+                  <path fill="none" d="M0 0h24v24H0z" />
                   <path
-                    d='M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8h4v2h-6V7h2v5z'
-                    fill='rgba(128,128,128,1)'
+                    d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8h4v2h-6V7h2v5z"
+                    fill="rgba(128,128,128,1)"
                   />
                 </svg>
                 <span>{props.lecturesTimes}</span>
@@ -110,15 +109,15 @@ const CardFormation = props => {
             </Link>
             <button>
               <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                width='18'
-                height='18'
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="18"
+                height="18"
               >
-                <path fill='none' d='M0 0h24v24H0z' />
+                <path fill="none" d="M0 0h24v24H0z" />
                 <path
-                  fill='#f7fafb'
-                  d='M4 6.414L.757 3.172l1.415-1.415L5.414 5h15.242a1 1 0 0 1 .958 1.287l-2.4 8a1 1 0 0 1-.958.713H6v2h11v2H5a1 1 0 0 1-1-1V6.414zM5.5 23a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm12 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z'
+                  fill="#f7fafb"
+                  d="M4 6.414L.757 3.172l1.415-1.415L5.414 5h15.242a1 1 0 0 1 .958 1.287l-2.4 8a1 1 0 0 1-.958.713H6v2h11v2H5a1 1 0 0 1-1-1V6.414zM5.5 23a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm12 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"
                 />
               </svg>
               <span>Acheter</span>
@@ -131,40 +130,37 @@ const CardFormation = props => {
         >
           {!selected && (
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='24'
-              height='24'
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
             >
-              <path fill='none' d='M0 0h24v24H0z' />
+              <path fill="none" d="M0 0h24v24H0z" />
               <path
-                d='M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z'
-                fill='rgba(128,128,128,1)'
+                d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z"
+                fill="rgba(128,128,128,1)"
               />
             </svg>
           )}
 
           {selected && (
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='24'
-              height='24'
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
             >
-              <path fill='none' d='M0 0h24v24H0z' />
+              <path fill="none" d="M0 0h24v24H0z" />
               <path
-                d='M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z'
-                fill='rgba(128,128,128,1)'
+                d="M12 10.828l-4.95 4.95-1.414-1.414L12 8l6.364 6.364-1.414 1.414z"
+                fill="rgba(128,128,128,1)"
               />
             </svg>
           )}
         </div>
       </div>
-      {/* <Link href={`/courses/${props.seoUrl}`}>
-        <a className={styles.link}>Détails</a>
-      </Link> */}
     </article>
-  )
-}
+  );
+};
 
-export default CardFormation
+export default CardFormation;
