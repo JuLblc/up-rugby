61faa137004f6092fd4405cffc2ec48d29ce3f32
JuLblc <julien.leblanc@epfedu.fr>
2022-06-05 09:57:01 +0200

    prevent no-admin to create exercice

--- pages/exercices/create-exercice.js
+++ pages/exercices/create-exercice.js
@@ -1,39 +1,69 @@
-import Exercice from "../../components/FormExercice/Exercice";
+import Exercice from '../../components/FormExercice/Exercice'
+import { getSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-const emptyExercice = {
-  title: "",
-  description: "",
-  seoUrl: "",
-  // set image by default
-  img: {
-    fileName: "bloc-1331.jpg",
-    url:
-      "https://res.cloudinary.com/dwznpgbcd/image/upload/v1644428743/uprugby-uploads-pict-formation/bloc-1331.jpg.jpg",
-    width: 768,
-    height: 450
-  },
-  chapters: [
-    {
-      title: "",
-      // lectures: [{ url: "", duration: 0 }],
-      lectures: [],
-      subchapters: []
+const NewExercice = props => {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!props.session) {
+      router.push('/login?login=signin')
+      return
     }
-  ],
-  isPublished: false
-};
+    if (props.session.user.role !== 'ADMIN') {
+      router.back()
+    }
+  }, [])
 
-const NewExercice = (props) => {
+  const emptyExercice = {
+    title: '',
+    description: '',
+    seoUrl: '',
+    // set image by default
+    img: {
+      fileName: 'defaut.jpeg',
+      url:
+        'https://res.cloudinary.com/uprugby/image/upload/v1654247035/uprugby-uploads-pict-formation/defaut.jpeg.jpg',
+      width: 768,
+      height: 450
+    },
+    chapters: [
+      {
+        title: '',
+        // lectures: [{ url: "", duration: 0 }],
+        lectures: [],
+        subchapters: []
+      }
+    ],
+    isPublished: false
+  }
+
   return (
     <main>
-      <h1>Ajouter Exercices</h1>
-      <Exercice
-        exerciceContent={emptyExercice}
-        action={"create"}
-        // disable={false}
-      />
-    </main>
-  );
-};
+      {props.session && props.session.user.role === 'ADMIN' && (
+        <>
+          <h1>Ajouter Exercices</h1>
 
-export default NewExercice;
+          <Exercice
+            exerciceContent={emptyExercice}
+            action={'create'}
+            disable={false}
+          />
+        </>
+      )}
+    </main>
+  )
+}
+
+export default NewExercice
+
+export const getServerSideProps = async context => {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
+}
